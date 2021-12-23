029561ed021d096e5f62d36870de14aced73e7a3
harryli0088 <harryli0088@gmail.com>
2021-12-22 23:39:46 -0500

    updated svelte config

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import static_adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,15 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: static_adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: process.env.NODE_ENV==="production" ? '/CS137-DNN-Project-StyleGAN2-Diamond' : undefined,
+    },
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
